a762b52a92a394c75030c63a4ab4ce4139c9560f
radinotech-beep <radinotech@gmail.com>
2026-05-24 01:36:57 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME='cafe-jangbu-20260524-event-time';
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(
+    fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request))
+  );
+});
